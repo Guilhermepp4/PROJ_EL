927158eb8bb9f51d6768ecd8ca096ddb55ddccca
Guilherme Pinho <guilhermepinho04@gmail.com>
2026-03-02 11:54:31 +0000

    adicionei o ficheiro txt

--- Projeto/grammar.py
+++ Projeto/grammar.py
@@ -31,37 +31,3 @@ T: /[a-z0-9+*\/:=;\[\]\(\)\'\"]+/
 # ARROW  = ->
 # EPS    = ε
 # """
-
-
-# gramatica_com_conflitos1 = """
-# lista -> '['']'
-#         | '[' Elems ']'
-
-# Elems -> Elems "," Elem
-#          | Elem
-
-# Elem -> int
-# """
-
-# gramatica_com_conflitos2 = """
-#     S -> '0' S '0'
-#     | '1' S '1'
-#     | '0'
-#     | '1'
-#     | 'e' "epsilon"
-# """
-
-
-# gramatica_trabalhar = """
-# Lista -> '[' Elems ']'
-
-# Elems -> e
-#     |   Elem ',' Elems
-
-# Elem -> int
-# """
-
-
-# Guia_Projeto = """
-# Linguagem -> parser(feito por nós) -> ASA(AST)
-# """
